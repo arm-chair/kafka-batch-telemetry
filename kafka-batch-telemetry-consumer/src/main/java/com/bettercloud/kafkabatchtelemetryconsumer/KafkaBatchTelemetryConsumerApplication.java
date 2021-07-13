e038972b5f7c1f0720b5fafcec222d4fcc29f9f6
Ken MacMurray <ken@bettercloud.com>
2021-07-13 15:38:09 -0400

    extract each message context and set scope before process

--- kafka-batch-telemetry-consumer/src/main/java/com/bettercloud/kafkabatchtelemetryconsumer/KafkaBatchTelemetryConsumerApplication.java
+++ kafka-batch-telemetry-consumer/src/main/java/com/bettercloud/kafkabatchtelemetryconsumer/KafkaBatchTelemetryConsumerApplication.java
@@ -1,14 +1,23 @@
 package com.bettercloud.kafkabatchtelemetryconsumer;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapGetter;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -38,7 +47,28 @@ public class KafkaBatchTelemetryConsumerApplication {
       messages.forEach(record -> {
         String message = record.value();
         System.out.println(message);
-        kafkaTemplate.send("final-topic", message);
+        TextMapGetter<ConsumerRecord<String, String>> getter =
+                new TextMapGetter<>() {
+                  @Override
+                  public String get(ConsumerRecord<String, String> carrier, String key) {
+                    Map<String, String> extracted = extractHeaders(carrier);
+                    if (extracted.containsKey(key)) {
+                      return extracted.get(key);
+                    }
+                    return null;
+                  }
+
+                  @Override
+                  public Iterable<String> keys(ConsumerRecord<String, String> carrier) {
+                    return StreamSupport.stream(carrier.headers().spliterator(), false).map(h -> h.key()).collect(Collectors.toList());
+                  }
+                };
+
+        Context extractedContext = GlobalOpenTelemetry.getPropagators().getTextMapPropagator()
+                .extract(Context.current(), record, getter);
+        try (Scope ignored = extractedContext.makeCurrent()) {
+          kafkaTemplate.send("final-topic", message);
+        }
       });
     }
   }
@@ -51,6 +81,17 @@ public class KafkaBatchTelemetryConsumerApplication {
     factory.setConsumerFactory(consumerFactory);
     factory.setBatchListener(true);
     return factory;
+  }
+
+  private Map<String, String> extractHeaders(ConsumerRecord record) {
+    Map<String, String> headers = new HashMap<>();
+    Arrays.stream(record.headers().toArray())
+            .forEach(header -> {
+              if (header.value() != null) {
+                headers.put(header.key(), new String(header.value(), StandardCharsets.UTF_8));
+              }
+            });
+    return headers;
   }
 
   public static void main(String[] args) {
